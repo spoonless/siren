c4d1030913fbe3ff41dcaa2d1571d63fcff6d7b0
David Gayerie <dagaydevel@free.fr>
2021-04-18 12:55:21 +0200

    removing experimental integration with fetch

--- index.js
+++ index.js
@@ -18,41 +18,6 @@ function createRequest(link, init) {
     return new Request(link.href, internalInit);
 }
 
-// TODO Check if headers are merged correctly or not.
-async function executeRequest(entity, method, init) {
-    let link = entity.link('self');
-    if (!$iren.isLink(link) && $iren.isSubEntityEmbeddedLink(entity)) {
-        link = entity;
-    } else {
-        throw new SirenError("Unable to load because it is not a sub entity and no self link found");
-    }
-    const request = createRequest(link, init);
-    const options = { method: method };
-    if (method === 'PUT') {
-        options.body = JSON.stringify(entity.properties);
-        options.headers = {
-            'Content-type': 'application/json'
-        }
-    }
-    const response = await fetch(request, options);
-    if (!response.ok) {
-        throw new SirenError(`Cannot load entity from ${request.url}. Status code is ${response.status}`);
-    }
-    const contentType = response.headers.get('Content-type');
-    if (contentType === 'application/json' || contentType === 'application/vnd.siren+json') {
-        const json = await response.json();
-        Object.keys(entity.properties).filter(k => !json.hasOwnProperty(k)).forEach(k => delete entity.properties[k]);
-        Object.assign(entity.properties, json.properties);
-        entity.properties[sirenSymbol] = new EntityWrapper(json);
-        entity.properties[sirenSymbol].rel = entity.rel;
-        entity.properties[sirenSymbol].href = entity.href;
-        json.properties = entity.properties;
-    } else if (method === 'GET') {
-        throw new SirenError(`Cannot load because of unexpected content-type ${application / json}`);
-    }
-    return entity.properties;
-}
-
 class EntityWrapper {
     constructor(e) {
         this[entitySymbol] = e;
@@ -159,14 +124,6 @@ class EntityWrapper {
 
     requests(param, init) {
         return this.links(param).map(l => createRequest(l, init));
-    }
-
-    async reload(init) {
-        return await executeRequest(this, 'GET', init);
-    }
-
-    async put(init) {
-        return await executeRequest(this, 'PUT', init);
     }
 }
 
