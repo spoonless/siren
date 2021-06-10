35862f8bc38ebd6299e3a565bb5b62c0c2489221
David <david@gayerie.dev>
2021-06-10 22:41:41 +0200

    fixing coverage on siren.js

--- .karma.conf.cjs
+++ .karma.conf.cjs
@@ -36,7 +36,8 @@ module.exports = function (config) {
       // source files, that you wanna generate coverage for
       // do not include tests or libraries
       // (these files will be instrumented by Istanbul)
-      'index.js': ['coverage']
+      'index.js': ['coverage'],
+      'siren.js': ['coverage'],
     },
 
     // optionally, configure the reporter
